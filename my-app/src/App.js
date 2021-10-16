67d5a5d329894a7e76b60f9dd4c43001a943fe4d
ErikTruong86 <eriktruong@csu.fullerton.edu>
2021-10-16 16:36:25 -0700

    Moving to different page
    We are able to move to a different page after clicking on the login button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,26 @@
 import Login from './containers/Login/Login';
 import './containers/Login/Login.css';
+import Candidates from "./containers/Candidates/Candidates";
+import Voted from './containers/Voted/Voted';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <Login/>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <Router>
+            <Switch>
+              <Route exact path="/candidates" component={Candidates}/>
+              <Route exact path="/voted" component={Voted} />
+              <Route path="/">
+                <Login />
+              </Route>
+            </Switch>
+          </Router>
+        </header>
+      </div>
   );
 }
 
---
+++ my-app/src/containers/Voted/Voted.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Voted = () => {
+  return (
+    <div>
+      <h1> JUST SOMETHING HERE TO SEE IF IT WORKS </h1>
+    </div>
+  );
+};
+
+export default Voted;
