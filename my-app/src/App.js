c1648390102e4151422942fb8eaf7dfc098f3049
Dalisa Nguyen <dalisan@csu.fullerton.edu>
2021-10-24 00:42:47 -0700

    candidate checkbox
    - each candidate card contains a button
    - clicked candidate is passed to review page also known as 'Voted.js'
    - candidate cards code is condensed
    - additional css is applied
    - back button on Voted.js routes back to candidates for re-selection

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,44 @@
-import Login from './containers/Login/Login';
-import './containers/Login/Login.css';
+import React, { useState } from "react";
+import Login from "./containers/Login/Login";
+import "./containers/Login/Login.css";
 import Candidates from "./containers/Candidates/Candidates";
-import Voted from './containers/Voted/Voted';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-
-
+import Voted from "./containers/Voted/Voted";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
+  const [candidateName, setCandidateName] = useState("");
+
+  const handleCandidate = (name) => {
+    console.log(`candidate ${name}`);
+    setCandidateName(name);
+};
+  // const [candidateName, setCandidateName] = useState({candidates_name:[]});
+
+  // const handleCandidate = (name, value) => {
+  //   console.log(`candidate ${value}`);
+  //   setCandidateName((preValue) => {
+  //     return {[name]: value}
+  //   });
+  // };
+
   return (
-      <div className="App">
-        <header className="App-header">
-          <Router>
-            <Switch>
-              <Route exact path="/candidates" component={Candidates}/>
-              <Route exact path="/voted" component={Voted} />
-              <Route path="/">
-                <Login />
-              </Route>
-            </Switch>
-          </Router>
-        </header>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <Router>
+          <Switch>
+            <Route exact path="/candidates">
+              <Candidates candidateNames={candidateName} handleCandidate={handleCandidate}/>
+            </Route>
+            <Route exact path="/voted">
+              <Voted candidateNames={candidateName}/>
+            </Route>
+            <Route path="/">
+              <Login />
+            </Route>
+          </Switch>
+        </Router>
+      </header>
+    </div>
   );
 }
 
--- my-app/src/containers/Voted/Voted.js
+++ my-app/src/containers/Voted/Voted.js
@@ -1,9 +1,26 @@
+import Button from "@restart/ui/esm/Button";
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-const Voted = () => {
+
+
+const Voted = (props) => {
+  const {candidateNames} = props;
+  let history = useHistory();
+
   return (
     <div>
       <h1> Displays candidate that the user voted for and contains back and submit button </h1>
+      {/* {candidateNames.candidates_name[0]} */}
+      {candidateNames}
+    <Button
+        block
+        size="lg"
+        type="submit"
+        onClick={() => history.push("/candidates")}
+      >
+        Back
+      </Button>
     </div>
   );
 };
